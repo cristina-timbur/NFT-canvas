21135d977903afc82df52b7874585292aa02e23d
mihai145 <opreamihai01@yahoo.com>
2024-05-11 00:27:35 +0300

    test colouring

--- hardhat/test/test.js
+++ hardhat/test/test.js
@@ -1,7 +1,16 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
 
-describe("Setup", function () {
+const SIZE = 5;
+const ROYALTY_PERCENT = 10;
+const TITLE = "Beautiful Canvas";
+const SAMPLES = 3;
+
+/*
+    Constructs a new canvas. Tests that the owner can color the pixels
+    and retrieve the correct color, while a non-owner may only read the pixels.
+*/
+describe("Coloring", function () {
     let users, owner;
     before(async function () {
         users = await ethers.getSigners();
@@ -10,23 +19,66 @@ describe("Setup", function () {
 
     let canvasContract;
     beforeEach(async function () {
-        let size = 10, royaltyPercent = 1;
         let canvasFactory = await ethers.getContractFactory("Canvas");
-        canvasContract = await canvasFactory.connect(owner).deploy(size, royaltyPercent);
+        canvasContract = await canvasFactory.connect(owner).deploy(
+            SIZE,
+            ROYALTY_PERCENT,
+            TITLE
+        );
         await canvasContract.deployed();
-    });
+    })
 
-    it("Should get the correct color of all cells", async function () {
-        for (let i = 0; i < 5; i++) {
-            for (let j = 0; j < 5; j++) {
-                // TODO: fix logic for getting the cell index from (i, j) coordinates
-                let colour = await canvasContract.connect(users[0]).getNFTColour(i * 5 + j);
-                expect(colour).to.have.members([0, 0, 0]);
+    it("Owner can write and read the color of all pixels", async function () {
+        for (let sample = 0; sample < SAMPLES; sample++) {
+            let i = Math.floor(Math.random() * SIZE);
+            let j = Math.floor(Math.random() * SIZE);
+
+            let pixel_idx = i * SIZE + j;
+            let r = Math.floor(Math.random() * 256),
+                g = Math.floor(Math.random() * 256),
+                b = Math.floor(Math.random() * 256);
+
+            let tx = await canvasContract.connect(owner).changeNFTColour(
+                pixel_idx, r, g, b, { gasLimit: 30000000 }
+            );
+            await tx.wait();
+
+            let colour = await canvasContract.connect(owner).getNFTColour(pixel_idx);
+            expect(colour).to.have.members([r, g, b]);
+        }
+    }).timeout(1000000);
+
+    it("Non-owner cannot write the color of any pixel", async function () {
+        for (let sample = 0; sample < SAMPLES; sample++) {
+            let i = Math.floor(Math.random() * SIZE);
+            let j = Math.floor(Math.random() * SIZE);
+
+            let pixel_idx = i * SIZE + j;
+            let r = Math.floor(Math.random() * 256),
+                g = Math.floor(Math.random() * 256),
+                b = Math.floor(Math.random() * 256);
+
+            let tx = await canvasContract.connect(users[1]).changeNFTColour(
+                pixel_idx, r, g, b, { gasLimit: 30000000 }
+            );
+
+            try {
+                await tx.wait();
+                expect(false);
+            } catch (err) {
+                expect(true);
             }
         }
-    });
+    }).timeout(1000000);
 
-    it("Description", async function () {
-        // TODO ...
-    });
+    it("Non-owner can read the color of all pixels", async function () {
+        for (let sample = 0; sample < SAMPLES; sample++) {
+            let i = Math.floor(Math.random() * SIZE);
+            let j = Math.floor(Math.random() * SIZE);
+
+            let pixel_idx = i * SIZE + j;
+            let colour = await canvasContract.connect(users[1]).getNFTColour(pixel_idx);
+            expect(colour).to.have.members([0, 0, 0]);
+        }
+    }).timeout(1000000);
 })
